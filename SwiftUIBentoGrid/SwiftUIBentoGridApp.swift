9ac9e26e4403373ca0f2007d3ee3c9781c446e33
Kyle <kylei.tw@gmail.com>
2024-08-16 17:08:26 +0800

    Initial Commit

---
+++ SwiftUIBentoGrid/SwiftUIBentoGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBentoGridApp.swift
+//  SwiftUIBentoGrid
+//
+//  Created by admin on 2024/8/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBentoGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
